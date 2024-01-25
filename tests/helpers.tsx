e6ffdf9c2cffbd6d67126fdb7c21ef2c2e89e6b1
redd1120 <monica.grevious@gmail.com>
2024-01-25 17:05:25 -0500

    Added tyes to HOC ReduxProvider props.

--- tests/helpers.tsx
+++ tests/helpers.tsx
@@ -1,6 +1,12 @@
+import { Store } from '@reduxjs/toolkit';
 import React from 'react';
 import { Provider } from 'react-redux';
 
-export const ReduxProvider = ({ children, reduxStore }) => (
+interface Props {
+  children: React.ReactNode;
+  reduxStore: Store;
+}
+
+export const ReduxProvider: React.FC<Props> = ({ children, reduxStore }) => (
   <Provider store={reduxStore}>{children}</Provider>
 );
