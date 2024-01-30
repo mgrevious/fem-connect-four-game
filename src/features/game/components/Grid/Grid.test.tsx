3427b819f69bf6557af3803b068f1b3ec5083fb3
redd1120 <monica.grevious@gmail.com>
2024-01-30 13:09:49 -0500

    Added helper function tests.

--- src/features/game/components/Grid/Grid.test.tsx
+++ src/features/game/components/Grid/Grid.test.tsx
@@ -1,18 +1,20 @@
-import { expect } from 'vitest';
+import { describe, expect } from 'vitest';
 import { fireEvent, screen } from '@testing-library/react';
 import { renderWithProviders } from '../../../../../test/utils';
 import Grid from '.';
 
-test('Grid renders', () => {
-  renderWithProviders(<Grid />);
-  expect(screen.getByTestId('grid')).toBeInTheDocument();
-});
-test('If Selector button is clicked, Grid renders game piece', async () => {
-  renderWithProviders(<Grid />);
-  expect(screen.getByTestId('grid')).toBeInTheDocument();
-  const selectedColumn = screen.getByTestId('selector-btn-0');
-  expect(selectedColumn).toBeInTheDocument();
+describe('Grid tests', () => {
+  test('Grid renders', () => {
+    renderWithProviders(<Grid />);
+    expect(screen.getByTestId('grid')).toBeInTheDocument();
+  });
+  test('If Selector button is clicked, Grid renders game piece', async () => {
+    renderWithProviders(<Grid />);
+    expect(screen.getByTestId('grid')).toBeInTheDocument();
+    const selectedColumn = screen.getByTestId('selector-btn-0');
+    expect(selectedColumn).toBeInTheDocument();
 
-  fireEvent.click(selectedColumn);
-  screen.getByTestId('active-gamepiece_06');
+    fireEvent.click(selectedColumn);
+    screen.getByTestId('active-gamepiece_06');
+  });
 });
---
+++ src/features/game/helpers.test.tsx
@@ -0,0 +1,76 @@
+import { describe } from 'vitest';
+import { PlayerColor, createGrid, findWinner } from './helpers';
+
+describe('Helper functions tests', () => {
+  test('findWinner returns false if no valid winner exists in gridMap', () => {
+    const gridMap = createGrid();
+    expect(findWinner(gridMap, PlayerColor.RED)).toBe(false);
+  });
+
+  test('findWinner returns true if gridMap has 4 consecutive vertical values for same player.', () => {
+    const grid = createGrid();
+    grid[0].rows.forEach((row, index) => {
+      if (index < 4) {
+        row.color = PlayerColor.RED;
+        row.selected = true;
+        row.highlight = true;
+      }
+    });
+
+    expect(findWinner(grid, PlayerColor.RED)).toBe(true);
+  });
+
+  test('findWinner returns true if gridMap has 4 consecutive horizontal values for same player.', () => {
+    const grid = createGrid();
+    grid[0].rows[5].color = PlayerColor.RED;
+    grid[0].rows[5].selected = true;
+    grid[0].rows[5].highlight = true;
+    grid[1].rows[5].color = PlayerColor.RED;
+    grid[1].rows[5].selected = true;
+    grid[1].rows[5].highlight = true;
+    grid[2].rows[5].color = PlayerColor.RED;
+    grid[2].rows[5].selected = true;
+    grid[2].rows[5].highlight = true;
+    grid[3].rows[5].color = PlayerColor.RED;
+    grid[3].rows[5].selected = true;
+    grid[3].rows[5].highlight = true;
+
+    expect(findWinner(grid, PlayerColor.RED)).toBe(true);
+  });
+
+  test('findWinner returns true if gridMap has 4 consecutive upward diagonal values for same player.', () => {
+    const grid = createGrid();
+    grid[0].rows[5].color = PlayerColor.RED;
+    grid[0].rows[5].selected = true;
+    grid[0].rows[5].highlight = true;
+    grid[1].rows[4].color = PlayerColor.RED;
+    grid[1].rows[4].selected = true;
+    grid[1].rows[4].highlight = true;
+    grid[2].rows[3].color = PlayerColor.RED;
+    grid[2].rows[3].selected = true;
+    grid[2].rows[3].highlight = true;
+    grid[3].rows[2].color = PlayerColor.RED;
+    grid[3].rows[2].selected = true;
+    grid[3].rows[2].highlight = true;
+
+    expect(findWinner(grid, PlayerColor.RED)).toBe(true);
+  });
+
+  test('findWinner returns true if gridMap has 4 consecutive downward diagonal values for same player.', () => {
+    const grid = createGrid();
+    grid[0].rows[2].color = PlayerColor.RED;
+    grid[0].rows[2].selected = true;
+    grid[0].rows[2].highlight = true;
+    grid[1].rows[3].color = PlayerColor.RED;
+    grid[1].rows[3].selected = true;
+    grid[1].rows[3].highlight = true;
+    grid[2].rows[4].color = PlayerColor.RED;
+    grid[2].rows[4].selected = true;
+    grid[2].rows[4].highlight = true;
+    grid[3].rows[5].color = PlayerColor.RED;
+    grid[3].rows[5].selected = true;
+    grid[3].rows[5].highlight = true;
+
+    expect(findWinner(grid, PlayerColor.RED)).toBe(true);
+  });
+});
